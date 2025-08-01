c8aa09007b4cda2e6d95b98c9e27996d1f1db7fa
Akihiko-Satou <a-satou@nagashima-s.co.jp>
2025-08-01 13:05:43 +0900

    ケーキクリック完了

---
+++ app2.py
@@ -0,0 +1,58 @@
+import pyautogui
+import time
+
+# screen_width, screen_height = pyautogui.size()
+# print(f"画面サイズ：{screen_width}×{screen_height}")
+
+# time.sleep(3)
+# pos = pyautogui.position()
+# print(f"現在のマウスの位置：{pos}")
+
+pyautogui.moveTo(138, 353, 1)
+pyautogui.click()
+
+pyautogui.moveTo(231, 354, 1)
+pyautogui.click()
+
+pyautogui.moveTo(312, 351, 1)
+pyautogui.click()
+
+pyautogui.moveTo(399, 349, 1)
+pyautogui.click()
+
+pyautogui.moveTo(509, 351, 1)
+pyautogui.click()
+
+pyautogui.moveTo(138, 422, 1)
+pyautogui.click()
+
+pyautogui.moveTo(228, 416, 1)
+pyautogui.click()
+
+pyautogui.moveTo(316, 416, 1)
+pyautogui.click()
+
+pyautogui.moveTo(397, 416, 1)
+pyautogui.click()
+
+pyautogui.moveTo(507, 416, 1)
+pyautogui.click()
+
+pyautogui.moveTo(143, 485, 1)
+pyautogui.click()
+
+pyautogui.moveTo(232, 485, 1)
+pyautogui.click()
+
+pyautogui.moveTo(307, 485, 1)
+pyautogui.click()
+
+pyautogui.moveTo(399, 485, 1)
+pyautogui.click()
+
+pyautogui.moveTo(503, 485, 1)
+pyautogui.click()
+
+pyautogui.moveTo(74, 69, 1)
+pyautogui.click()
+
